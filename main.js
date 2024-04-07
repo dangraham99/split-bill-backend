0ef733f8ce738f2042b68c445b592089e52a95a8
Dan <36828197+dangraham99@users.noreply.github.com>
2024-04-07 13:30:41 +0100

    added create group route and connecting admin user

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
 const app = express()
+app.use(express.json())
 const port = 3000
 const prisma = new PrismaClient()
 
 app.get('/user/create', async (req, res) => {
 
-    const createdUser = await prisma.user.create({
+    const createdUser = await prisma.users.create({
         data: {
             name: faker.person.fullName(),
             email: faker.internet.email(),
@@ -19,9 +21,27 @@ app.get('/user/create', async (req, res) => {
 
 })
 
+app.post('/group/create', async(req, res) => {
+    console.log(req.body)
+
+    const createdGroup = await prisma.groups.create({
+        data:{
+            title: req.body.title,
+            users: {
+                create: [{userId: req.body.admin, admin: true}]
+            }
+        },
+    })
+    res.send(createdGroup)
+})
+
 
 app.get('/', async (req, res) => {
-    const users = await prisma.user.findMany()
+    const users = await prisma.users.findMany({
+        include: {
+            groups: true
+        }
+    })
 
     res.send(users)
 })
