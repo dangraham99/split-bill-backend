7242dc41a01305b2e6f90ac1ee0801118ca97f8b
Dan <36828197+dangraham99@users.noreply.github.com>
2024-04-07 22:21:36 +0100

    placeholders

---
+++ src/controllers/group.controller.js
@@ -0,0 +1,21 @@
+async function show(req) {
+  
+}
+
+async function create(req){
+
+}
+
+async function read(req) {
+
+}
+
+async function update(req) {
+
+}
+
+async function del(req) {
+
+}
+
+export { show, create, read, update, del }
